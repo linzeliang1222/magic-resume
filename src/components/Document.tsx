d4bc96577db2d09e730db76d9808b9ca33b5956b
zller <linzeliang1222@gmail.com>
2025-07-15 15:13:20 +0800

    feat: add analytics script

--- src/components/Document.tsx
+++ src/components/Document.tsx
@@ -16,6 +16,7 @@ export default function Document({ children, locale, bodyClassName }: Props) {
     <html className={inter.className} lang={locale} suppressHydrationWarning>
       <head>
         <link rel="icon" href="/favicon.ico?v=2" />
+        <script defer src="https://umami.zller.cc/script.js" data-website-id="3a5a4017-dfbd-497b-9565-d459e9d4a27f" data-domains="resume.zller.cn"></script>
       </head>
       <body className={bodyClassName}>{children}</body>
     </html>
