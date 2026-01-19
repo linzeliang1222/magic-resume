09bc425f8b356e6652fff77318792ca3a59451d7
zller <linzeliang1222@gmail.com>
2026-01-20 03:53:08 +0800

    chore(analytics): 更新统计域名为 resume.rayinlab.cn

--- src/components/Document.tsx
+++ src/components/Document.tsx
@@ -16,7 +16,7 @@ export default function Document({ children, locale, bodyClassName }: Props) {
     <html className={inter.className} lang={locale} suppressHydrationWarning>
       <head>
         <link rel="icon" href="/favicon.ico?v=2" />
-        <script defer src="https://umami.zller.cc/script.js" data-website-id="8f8ae41c-68a0-498a-879b-5a68c94781e1" data-domains="resume.zller.cn"></script>
+        <script defer src="https://umami.zller.cc/script.js" data-website-id="8f8ae41c-68a0-498a-879b-5a68c94781e1" data-domains="resume.rayinlab.cn"></script>
       </head>
       <body className={bodyClassName}>{children}</body>
     </html>
